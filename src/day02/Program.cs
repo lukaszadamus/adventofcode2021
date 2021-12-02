1ac58c8f4b387a32c1e0847d0bc5cc70a14cf644
lukaszadamus <lukasz.adamus@soodo.pl>
2021-12-02 12:15:28 +0100

    Add tracking (just in case)

--- src/day02/Program.cs
+++ src/day02/Program.cs
@@ -5,38 +5,35 @@
     return new Instruction(operation, int.Parse(line[1]));
 }).ToArray();
 
+var routeA = new Tracker(Tracker.PositioningA).Track(input);
 
-var horizontal = input.Where(x => x.Operation == Operation.forward)
-    .Select(x => x.Value)
-    .Sum();
-var depth = input.Where(x => x.Operation != Operation.forward)
-    .Select(x => x.Operation == Operation.down ? x.Value : -x.Value)
-    .Sum();
+Console.WriteLine($"Result A: {routeA.Last().Horizontal * routeA.Last().Depth}");
 
-Console.WriteLine($"Result A: {horizontal * depth}");
+var routeB = new Tracker(Tracker.PositioningB).Track(input);
 
-horizontal = 0;
-depth = 0;
-var aim = 0;
-foreach (var i in input)
+Console.WriteLine($"Result B: {routeB.Last().Horizontal * routeB.Last().Depth}");
+
+public class Tracker
 {
-    switch (i.Operation)
+    private static Position _initialPosition = new Position(0, 0, 0);
+    private static ICollection<Position> _track = new List<Position>() { _initialPosition };
+    private readonly Func<Position, Instruction, Position> _instructionProcessor;
+    private readonly Func<Position, IEnumerable<Position>> _resultSelector = result => _track;
+
+    public Tracker(Func<Instruction, Position, Position> positioning)
     {
-        case Operation.down:
-            aim += i.Value;
-            break;
-        case Operation.up:
-            aim -= i.Value;
-            break;
-        case Operation.forward:
-            horizontal += i.Value;
-            depth += aim * i.Value;
-            break;
+        _instructionProcessor = (acc, x) =>
+        {
+            var p = positioning(x, acc);
+            _track.Add(p);
+            return p;
+        };
     }
-}
 
-Console.WriteLine($"Result B: {horizontal * depth}");
+    public Position[] Track(IEnumerable<Instruction> instructions)
+        => instructions.Aggregate(_initialPosition, _instructionProcessor, _resultSelector).ToArray();
 
+<<<<<<< Updated upstream
 public record Instruction(Operation Operation, int Value);
 public record Position(int Horizontal, int Depth, int Aim);
 public enum Operation
@@ -44,5 +41,54 @@ public enum Operation
     forward,
     down,
     up,
+=======
+    public static Func<Instruction, Position, Position> PositioningA = (i, p) =>
+    {
+        var horizontal = p.Horizontal;
+        var depth = p.Depth;
+        var aim = p.Aim;
+
+        switch (i.Operation)
+        {
+            case Operation.down:
+                depth += i.Value;
+                break;
+            case Operation.up:
+                depth -= i.Value;
+                break;
+            case Operation.forward:
+                horizontal += i.Value;                
+                break;
+        }
+
+        return new Position(horizontal, depth, aim);
+    };
+
+    public static Func<Instruction, Position, Position> PositioningB = (i, p) =>
+    {
+        var horizontal = p.Horizontal;
+        var depth = p.Depth;
+        var aim = p.Aim;
+
+        switch (i.Operation)
+        {
+            case Operation.down:
+                aim += i.Value;
+                break;
+            case Operation.up:
+                aim -= i.Value;
+                break;
+            case Operation.forward:
+                horizontal += i.Value;
+                depth += aim * i.Value;
+                break;
+        }
+
+        return new Position(horizontal, depth, aim);
+    };
+>>>>>>> Stashed changes
 }
 
+public record Instruction(Operation Operation, int Value);
+public record Position(int Horizontal, int Depth, int Aim);
+public enum Operation { forward, down, up }
